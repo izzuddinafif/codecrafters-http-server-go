50a2f1f02884252bafca46cd5ab0b938387f4a6a
afif <izzuddinafif@gmail.com>
2024-07-31 22:18:46 +0700

    pass stage

--- app/server.go
+++ app/server.go
@@ -6,11 +6,15 @@ import (
 	"os"
 	"log"
 	"strings"
+	"flag"
+	"path/filepath"
 )
 
 func main() {
 	fmt.Println("This is my own HTTP Server, It's now up and running! :D")
-
+	var dir string
+	flag.StringVar(&dir, "directory", "/tmp/", "Directory to write/serve the file from")
+	flag.Parse()
 	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		log.Println("Failed to bind to port 4221: ", err)
@@ -24,15 +28,16 @@ func main() {
 			log.Println("Error accepting connection: ", err)
 			continue
 		}
-		go handleClient(conn) // Handle a client connection
+		go handleClient(conn, dir) // Handle a client connection
 	}
 }
 
-func handleClient(conn net.Conn) {
+func handleClient(conn net.Conn, dir string) {
 	defer conn.Close() // Ensure we terminate the connection after we're done
 	var (
 		err error
 		res, msg, path, userAgent string
+		content []byte
 		fullReq, req []string
 		n int
 	)
@@ -84,6 +89,28 @@ func handleClient(conn net.Conn) {
 			return
 		}
 		log.Println("Response sent: \"", res, "\"")
+	case strings.HasPrefix(path, "/files/"):
+		fileName, _ := strings.CutPrefix(path, "/files/") // Extract the filename
+		filePath := filepath.Join(dir, fileName)
+		_, err := os.Stat(filePath)
+		if err == nil {
+			content, err = os.ReadFile(filePath)
+			if err != nil {
+				log.Println("Failed to open file: ", err)
+				return
+			}
+			res = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(content), string(content))
+		} 
+		if os.IsNotExist(err) {
+			res = "HTTP/1.1 404 Not Found\r\n\r\n"
+		}
+		_, err = conn.Write([]byte(res))
+			if err != nil {
+				log.Println("Error writing response: ", err)
+				return
+			}
+			log.Println("Response sent: \"", res, "\"")
+		
 	default:
 		res = "HTTP/1.1 404 Not Found\r\n\r\n"
 		_, err = conn.Write([]byte(res))
